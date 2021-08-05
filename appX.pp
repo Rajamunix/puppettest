2c51702c70155e1e08ba6f1bbbd15422385993b6
Rajamunix <88448054+Rajamunix@users.noreply.github.com>
2021-08-05 03:39:37 -0400

    Update appX.pp

--- appX.pp
+++ appX.pp
@@ -1,1 +1,34 @@
-
+init.pp -->
+class appX {
+  contain appX1::config_file
+  contain appX1::service
+  }
+  
+  conifg_file
+  
+  class appX::config_file ($String $mypackage
+  $String $ensure){
+    package {'$mypackage':
+      name => '$mypackage',
+      ensure => $ensure,
+      }
+    file {'/etc/init.d/appX':
+      ensure => present,
+      content => 'puppet:///module/appX/files/abc.txt',
+      }
+      
+    -----
+    server.pp
+    class appX::service ($String $service
+    $String $ensure) {
+      service {'$service':
+        ensure => $ensure,
+        enable => true,
+        
+       }
+       
+       common.yaml
+       appX::config_file:mypackage: appX
+       appX::config_file:ensure: 1.0
+       appX::service::service: appX
+       appX::service::ensure: on
